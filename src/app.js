c1c55fa23ba277f2cf7e34c3d2e5a243927e122a
Raka Nugraha <rakanuarinugraha@gmail.com>
2026-02-17 10:53:19 +0700

    Update: Fix app.js static path

--- src/app.js
+++ src/app.js
@@ -11,11 +11,11 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Static folder
-app.use(express.static(path.join(__dirname, "views")));
+app.use(express.static(path.join(__dirname, "public")));
 
 // View engine
 app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "public"));
+app.set("views", path.join(__dirname, "views"));
 
 hbs.registerPartials(path.join(__dirname, "views/partials"));
 
