7a6aceee734c309d44fababf6c12dfb21fed2a43
Raka Nugraha <rakanuarinugraha@gmail.com>
2026-02-17 09:44:43 +0700

    Update app.js static path

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Static folder
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(process.cwd(), "public")));
 
 // View engine
 app.set("view engine", "hbs");
